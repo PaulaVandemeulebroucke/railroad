4ce990ca28d263c409d442794a682b2528810068
Benoit Morin <benoit.morin@hec.edu>
2018-03-13 11:14:21 +0100

    first setting of policy

---
+++ app/policies/offer_policy.rb
@@ -0,0 +1,31 @@
+class OfferPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    is_user_the_creator?
+  end
+
+  def destroy?
+    is_user_the_creator?
+  end
+
+  private
+
+  def is_user_the_creator?
+    record.user == user
+    # - offer: the offer passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+end
